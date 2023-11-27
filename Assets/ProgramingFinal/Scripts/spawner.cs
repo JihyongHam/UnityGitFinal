8d9508dcc4fb33fdccd2ac37cdb468a09420041b
Jihyong Ham <hamjihyoung@gmail.com>
2023-11-27 17:44:36 -0600

    Random Enemy / Gizmo Display / Boss
    Random Enemy / Gizmo Display
    
    Random Enemy
    - spawn delay
    - spawn  randomization
    - bool boss or not
    - boss respawn condition: removing all the enemy when boss appear
    
    Gizmo Display scripts
    - giving visual color

--- Assets/ProgramingFinal/Scripts/spawner.cs
+++ Assets/ProgramingFinal/Scripts/spawner.cs
@@ -7,9 +7,13 @@ public class spawner : MonoBehaviour
     [Header("Spawner Settings")]
     [SerializeField] private float spawnRate = 1f;
     [SerializeField] private bool canSpawn = true;
-
     [SerializeField] private GameObject[] enemyPrefab;
-    
+    [SerializeField] private bool spawnBossOrNot = true;
+    [SerializeField] private GameObject bossPrefab;
+    [SerializeField] private int enemiesSpawnBeforeBoss = 0;
+
+    private int enemiesSpawned = 0;
+
 
     [Header("Gizmo Settings")]
     [SerializeField] private Color gizmoColor = Color.clear;
@@ -33,6 +37,23 @@ public class spawner : MonoBehaviour
             GameObject enemyToSpawn = enemyPrefab[rand];
 
             Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+
+            enemiesSpawned++;
+
+            if (enemiesSpawned >= enemiesSpawnBeforeBoss && spawnBossOrNot)
+            {
+                yield return new WaitForSeconds(spawnRate);
+
+                Instantiate(bossPrefab, transform.position, Quaternion.identity);
+
+                enemiesSpawned = 0;
+
+                foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
+                {
+                    canSpawn = false;
+                    Destroy(enemyInstance);
+                }
+            }
         }
     }
 
