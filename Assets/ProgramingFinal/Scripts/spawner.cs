338e0a6dcdb1b448c1bc484bca26c7ae0d62f759
Jihyong Ham <hamjihyoung@gmail.com>
2023-11-28 02:20:03 -0600

    Respawn Activator
    Added respawn activator

--- Assets/ProgramingFinal/Scripts/spawner.cs
+++ Assets/ProgramingFinal/Scripts/spawner.cs
@@ -1,79 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using TMPro;
 
-
-public class spawner : MonoBehaviour
-{
-    [Header("Spawner Settings")]
-    [SerializeField] private float spawnRate = 1f;
-    [SerializeField] private bool canSpawn = true;
-    [SerializeField] private bool spawnBossOrNot = true;
-    [SerializeField] private int howManyEnemiesBeforeBoss = 1;
-
-    [Header("" + "")]
-    [SerializeField] private GameObject[] enemyPrefab;
-    [SerializeField] private GameObject bossPrefab;
-
-
-    private int enemiesSpawned = 1;
-
-    [Header("" + "")]
-    [Header("Gizmo Settings")]
-    [SerializeField] private Color gizmoColor = Color.clear;
-    private float gizmoRadius = 1.0f;
-
-
-    private void Start()
+    public class spawner : MonoBehaviour
     {
-        StartCoroutine(Spawner());
-    }
+        [Header("Spawner Settings")]
+        [SerializeField] private float spawnRate = 1f;
+        [SerializeField] private bool canSpawn = false;
+        [SerializeField] private bool spawnBossOrNot = true;
+        [SerializeField] private int howManyEnemiesBeforeBoss = 1;
 
-    private IEnumerator Spawner()
-    {
-        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        [Header("" + "")]
+        [SerializeField] private GameObject[] enemyPrefab;
+        [SerializeField] private GameObject bossPrefab;
 
-        // call UIcontoroller class
-        UIController uiControllerInstance = FindObjectOfType<UIController>();
 
-        while (canSpawn)
+        private int enemiesSpawned = 1;
+
+
+        [Header("" + "")]
+        [Header("Gizmo Settings")]
+        [SerializeField] private Color gizmoColor = Color.green;
+        private float gizmoRadius = 1.0f;
+
+
+        private void Start()
         {
-            yield return wait;
+            StartCoroutine(Spawner());
+        }
 
-            int rand = Random.Range(0, enemyPrefab.Length);
-            GameObject enemyToSpawn = enemyPrefab[rand];
-
-            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
-
-            enemiesSpawned++;
-
-            // UIcontoroller
-            if (uiControllerInstance != null)
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
             {
-                uiControllerInstance.IncrementEnemyCount();
+                canSpawn = true; 
+                StartCoroutine(Spawner());
             }
+        }
 
+        private IEnumerator Spawner()
+        {
+            WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
-            if (enemiesSpawned >= howManyEnemiesBeforeBoss && spawnBossOrNot)
+            // call UIcontoroller class
+            UIController uiControllerInstance = FindObjectOfType<UIController>();
+
+            while (canSpawn)
             {
-                yield return new WaitForSeconds(spawnRate);
+                yield return wait;
 
-                Instantiate(bossPrefab, transform.position, Quaternion.identity);
- 
-                foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
+                int rand = Random.Range(0, enemyPrefab.Length);
+                GameObject enemyToSpawn = enemyPrefab[rand];
+
+                Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+
+                enemiesSpawned++;
+
+                // UIcontoroller
+                if (uiControllerInstance != null)
                 {
-                    canSpawn = false;
-                    Destroy(enemyInstance);
+                    uiControllerInstance.IncrementEnemyCount();
                 }
-            }
 
+
+                if (enemiesSpawned >= howManyEnemiesBeforeBoss && spawnBossOrNot)
+                {
+                    yield return new WaitForSeconds(spawnRate);
+
+                    Instantiate(bossPrefab, transform.position, Quaternion.identity);
+ 
+                    foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
+                    {
+                        canSpawn = false;
+                        Destroy(enemyInstance);
+                    }
+                }
+
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawWireSphere(transform.position, gizmoRadius);
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = gizmoColor;
-        Gizmos.DrawWireSphere(transform.position, gizmoRadius);
-    }
-}
