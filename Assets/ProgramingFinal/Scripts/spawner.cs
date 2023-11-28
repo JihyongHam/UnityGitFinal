c5c16645c71df0c0df98e2d6f2b494965762e155
Jihyong Ham <hamjihyoung@gmail.com>
2023-11-28 03:35:36 -0600

    Added Audio / reorganized spawner script
    Added music for specific scenarios for boss fight
    
    *Almost done

--- Assets/ProgramingFinal/Scripts/spawner.cs
+++ Assets/ProgramingFinal/Scripts/spawner.cs
@@ -1,89 +1,120 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
-    public class spawner : MonoBehaviour
+public class spawner : MonoBehaviour
+{
+    [Header("Spawner Settings")]
+    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private bool canSpawn = false;
+    [SerializeField] private bool spawnBossOrNot = true;
+    [SerializeField] private int howManyEnemiesBeforeBoss = 1;
+    [SerializeField] private int maxEnemiesEnemySpawn = 0;
+
+    [Header("" + "")]
+    [SerializeField] private GameObject[] enemyPrefab;
+    [SerializeField] private GameObject[] bossPrefab;
+
+
+    private int enemiesSpawned = 1;
+
+
+    [Header("" + "")]
+    [Header("Gizmo Settings")]
+    [SerializeField] private Color gizmoColor = Color.green;
+    private float gizmoRadius = 1.0f;
+
+    [Header("" + "")]
+    [Header("Music Settings")]
+    [SerializeField] private AudioClip mainMusic;
+    [SerializeField] private AudioClip bossMusic;
+        
+    private AudioSource audioSource;
+
+
+    private void Start()
     {
-        [Header("Spawner Settings")]
-        [SerializeField] private float spawnRate = 1f;
-        [SerializeField] private bool canSpawn = false;
-        [SerializeField] private bool spawnBossOrNot = true;
-        [SerializeField] private int howManyEnemiesBeforeBoss = 1;
-        [SerializeField] private int maxEnemiesEnemySpawn = 0;
+        StartCoroutine(Spawner());
 
-        [Header("" + "")]
-        [SerializeField] private GameObject[] enemyPrefab;
-        [SerializeField] private GameObject bossPrefab;
+        audioSource = GetComponent<AudioSource>();
+        PlayMainMusic();
+    
+    }
 
-
-        private int enemiesSpawned = 1;
-
-
-        [Header("" + "")]
-        [Header("Gizmo Settings")]
-        [SerializeField] private Color gizmoColor = Color.green;
-        private float gizmoRadius = 1.0f;
-
-
-        private void Start()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
+            canSpawn = true; 
             StartCoroutine(Spawner());
         }
+    }
 
-        private void OnTriggerEnter(Collider other)
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
+        // call UIcontoroller class
+        UIController uiControllerInstance = FindObjectOfType<UIController>();
+
+        while (canSpawn && enemiesSpawned <= maxEnemiesEnemySpawn)
         {
-            if (other.CompareTag("Player"))
+            yield return wait;
+
+            int randomOne = Random.Range(0, enemyPrefab.Length);
+            GameObject enemyToSpawn = enemyPrefab[randomOne];
+
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+
+            enemiesSpawned++;
+
+            // UIcontoroller
+            if (uiControllerInstance != null)
             {
-                canSpawn = true; 
-                StartCoroutine(Spawner());
+                uiControllerInstance.IncrementEnemyCount();
             }
-        }
 
-        private IEnumerator Spawner()
-        {
-            WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
-            // call UIcontoroller class
-            UIController uiControllerInstance = FindObjectOfType<UIController>();
-
-            while (canSpawn && enemiesSpawned <= maxEnemiesEnemySpawn)
+            if (enemiesSpawned >= howManyEnemiesBeforeBoss && spawnBossOrNot)
             {
                 yield return wait;
+                int randomTwo = Random.Range(0, bossPrefab.Length);
+                GameObject bossToSpawn = bossPrefab[randomTwo];
 
-                int rand = Random.Range(0, enemyPrefab.Length);
-                GameObject enemyToSpawn = enemyPrefab[rand];
+                audioSource.Stop();
 
-                Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+                Instantiate(bossToSpawn, transform.position, Quaternion.identity);
 
-                enemiesSpawned++;
-
-                // UIcontoroller
-                if (uiControllerInstance != null)
+                if (audioSource != null && bossMusic != null)
                 {
-                    uiControllerInstance.IncrementEnemyCount();
+                        audioSource.clip = bossMusic;
+                        audioSource.Play();
                 }
-
-
-                if (enemiesSpawned >= howManyEnemiesBeforeBoss && spawnBossOrNot)
-                {
-                    yield return new WaitForSeconds(spawnRate);
-
-                    Instantiate(bossPrefab, transform.position, Quaternion.identity);
  
-                    foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
-                    {
-                        canSpawn = false;
-                        Destroy(enemyInstance);
-                    }
+                foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
+                {
+                    canSpawn = false;
+                    Destroy(enemyInstance);
                 }
-
             }
-        }
 
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = gizmoColor;
-            Gizmos.DrawWireSphere(transform.position, gizmoRadius);
         }
     }
+    private void PlayMainMusic()
+    {
+        if (audioSource != null && mainMusic != null)
+        {
+            audioSource.clip = mainMusic;
+            audioSource.Play(); 
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoRadius);
+    }
+
+}
