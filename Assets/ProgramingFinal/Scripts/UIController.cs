e0cfeeb6202dcc660d9c363a28ca3186ad2c00f1
Jihyong Ham <hamjihyoung@gmail.com>
2023-11-28 04:26:51 -0600

    Reorganized the script
    Reorganized the script

--- Assets/ProgramingFinal/Scripts/UIController.cs
+++ Assets/ProgramingFinal/Scripts/UIController.cs
@@ -5,27 +5,38 @@ using TMPro;
 
 public class UIController : MonoBehaviour
 {
+    // UI Settings
     [Header("UI Settings")]
+    // Should assign enemycount texts on canvas
     [SerializeField] private TextMeshProUGUI enemyCountText;
+    // Can adjust text message
     [SerializeField] private string enemyCountTextMessage = "Enemy Count: ";
 
+    // Enemy counts for UI script
     private int totalEnemiesSpawned = 0;
 
+    // count enemy spawning when game starts
     private void Start()
     {
         UpdateEnemyCountText();
     }
 
+    // Increment enemy counts
     public void IncrementEnemyCount()
     {
+        // totalEnemiesSpawned = totalEnemiesSpawned + 1;
         totalEnemiesSpawned++;
+
+        // Update enemy counts
         UpdateEnemyCountText();
     }
 
+    // Enemy counts fucntion
     private void UpdateEnemyCountText()
     {
         if (enemyCountText != null)
         {
+            // Number UI changes only
             enemyCountText.text = enemyCountTextMessage + totalEnemiesSpawned.ToString();
         }
     }
--- Assets/ProgramingFinal/Scripts/spawner.cs
+++ Assets/ProgramingFinal/Scripts/spawner.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class spawner : MonoBehaviour
 {
+    // Spawner settings
     [Header("Spawner Settings")]
     [SerializeField] private float spawnRate = 1f;
     [SerializeField] private bool canSpawn = false;
@@ -12,19 +12,24 @@ public class spawner : MonoBehaviour
     [SerializeField] private int howManyEnemiesBeforeBoss = 1;
     [SerializeField] private int maxEnemiesEnemySpawn = 0;
 
+    // Enemy sort Settings
     [Header("" + "")]
     [SerializeField] private GameObject[] enemyPrefab;
     [SerializeField] private GameObject[] bossPrefab;
 
 
+    // Enemy counts for spawner script
     private int enemiesSpawned = 1;
 
 
+    // Gizmo settings
     [Header("" + "")]
     [Header("Gizmo Settings")]
     [SerializeField] private Color gizmoColor = Color.green;
-    private float gizmoRadius = 1.0f;
+    [SerializeField] private float gizmoRadius = 1.0f;
 
+
+    // Music settings
     [Header("" + "")]
     [Header("Music Settings")]
     [SerializeField] private AudioClip mainMusic;
@@ -35,74 +40,99 @@ public class spawner : MonoBehaviour
 
     private void Start()
     {
+        // spawning starts
         StartCoroutine(Spawner());
 
+        // music starts
         audioSource = GetComponent<AudioSource>();
-        PlayMainMusic();
+        PlayMusic();
     
     }
 
+    // Trigger Volume
     private void OnTriggerEnter(Collider other)
     {
+        // Trigger when player enter
         if (other.CompareTag("Player"))
-        {
-            canSpawn = true; 
+        {   
+            // canSpawn should check to false to use trigger volume
+            canSpawn = true;
+            // spawning starts for trigger volume
             StartCoroutine(Spawner());
         }
     }
 
+    // IEnumerator spawner method
     private IEnumerator Spawner()
     {
+        // wait for second based on spawnRate assigned by me for next enemy spawn
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
         // call UIcontoroller class
         UIController uiControllerInstance = FindObjectOfType<UIController>();
 
+        // looping condition canSpawn should be turned on
         while (canSpawn && enemiesSpawned <= maxEnemiesEnemySpawn)
         {
             yield return wait;
 
+            // randomizing enemies spawning
             int randomOne = Random.Range(0, enemyPrefab.Length);
             GameObject enemyToSpawn = enemyPrefab[randomOne];
 
+            // enemy spawns
             Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
 
+            // enemiesSpawned = enemiesSpawned + 1, which is caculating
             enemiesSpawned++;
 
-            // UIcontoroller
+            // About UIcontoroller script
             if (uiControllerInstance != null)
             {
+                // call IncrementEnemyCount() script inside of the UIController
                 uiControllerInstance.IncrementEnemyCount();
             }
 
-
+            // looping condition spawnBossOrNot should be turned on
             if (enemiesSpawned >= howManyEnemiesBeforeBoss && spawnBossOrNot)
             {
                 yield return wait;
+
+                // randomizing enemies spawning
                 int randomTwo = Random.Range(0, bossPrefab.Length);
                 GameObject bossToSpawn = bossPrefab[randomTwo];
 
+                // audio should stop to get new audio for boss
                 audioSource.Stop();
 
+                // boss spawns
                 Instantiate(bossToSpawn, transform.position, Quaternion.identity);
 
+                // play boss music
                 if (audioSource != null && bossMusic != null)
                 {
                         audioSource.clip = bossMusic;
                         audioSource.Play();
                 }
- 
+                
+                // when boss appears, all the enemies are removed
                 foreach (GameObject enemyInstance in GameObject.FindGameObjectsWithTag("Enemy"))
                 {
+                    // to avoid additional respawns for enemies
                     canSpawn = false;
+                    
+                    // destory enmies taged with Enemy
                     Destroy(enemyInstance);
                 }
             }
 
         }
     }
-    private void PlayMainMusic()
+
+    // play music function
+    private void PlayMusic()
     {
+        // main music play
         if (audioSource != null && mainMusic != null)
         {
             audioSource.clip = mainMusic;
@@ -111,6 +141,7 @@ public class spawner : MonoBehaviour
     }
 
 
+    // gizmo function
     private void OnDrawGizmos()
     {
         Gizmos.color = gizmoColor;
